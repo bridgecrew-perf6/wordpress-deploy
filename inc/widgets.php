50f808e77cc9c2ae5d8e10a9db577956555107f5
Federico Ramírez <00009220@uca.edu.sv>
2022-06-16 22:13:55 -0600

    Changing template 3.0

--- inc/widgets.php
+++
@@ -1,6 +0,0 @@
-<?php
-get_template_part( 'widgets/contact-info' );
-get_template_part( 'widgets/video-widget' );
-get_template_part( 'widgets/widget-mt-social' );
-get_template_part( 'widgets/widget-mt-cta' );
-get_template_part( 'widgets/widget-mt-home-news' );
